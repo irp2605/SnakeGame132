b5281e8024a371aa8e1a659ecf80ac8d6663cc97
aditrigadigi <153329331+aditrigadigi@users.noreply.github.com>
2024-03-03 17:30:10 -0500

    new inner class + javadoc comments

--- Snake/src/game/Fruit.java
+++ Snake/src/game/Fruit.java
@@ -2,7 +2,21 @@ package game;
 
 import java.awt.Graphics;
 
+/**
+ * Defines the functionality of Fruits (Apple and Orange).
+ * 
+ * @author Aditri Gadigi and Ishan Patel
+ *
+ */
 public interface Fruit {
-    public void paint(Graphics brush);
-    // add fruit-specific methods
+	/**
+	 * Draw the Fruit object. The method loops through the list of points and
+	 * separates them into a list of integer x coordinates and integer y
+	 * coordinates. These points are used to draw the polygon. The brush color is
+	 * selected before the polygon is drawn. The brush color is different for each
+	 * fruit.
+	 * 
+	 * @param brush
+	 */
+	public void paint(Graphics brush);
 }
--- Snake/src/game/Orange.java
+++ Snake/src/game/Orange.java
@@ -1,13 +1,38 @@
 package game;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
-public class Orange extends Polygon implements Fruit{
+/**
+ * This class extends Polygon and implements the Fruit interface. This class
+ * represents the Orange object which appears in the game.
+ * 
+ * @author Aditri Gadigi and Ishan Patel
+ */
+public class Orange extends Polygon implements Fruit {
 
+	private static Color color = Color.orange;
+
+	/**
+	 * Defines an Orange. It relies on the super class constructor to define the
+	 * shape.
+	 * 
+	 * @param inShape
+	 * @param inPosition
+	 * @param inRotation
+	 */
 	public Orange(Point[] inShape, Point inPosition, double inRotation) {
 		super(inShape, inPosition, inRotation);
 	}
 
+	/**
+	 * Draw the Orange object. The method loops through the list of points and
+	 * separates them into a list of integer x coordinates and integer y
+	 * coordinates. These points are used to draw the polygon. The brush color is
+	 * selected to be orange before the polygon is drawn.
+	 * 
+	 * @param brush
+	 */
 	public void paint(Graphics brush) {
 		int nPoints = this.getPoints().length;
 		int[] xPoints = new int[nPoints];
@@ -17,6 +42,7 @@ public class Orange extends Polygon implements Fruit{
 			xPoints[i] = (int) points[i].getX();
 			yPoints[i] = (int) points[i].getY();
 		}
+		brush.setColor(color);
 		brush.fillPolygon(xPoints, yPoints, nPoints);
 	}
 
